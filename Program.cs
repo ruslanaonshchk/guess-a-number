2b5e31fad2b798dab5eef23d5cb16f86c5b00a04
Ruslana Onyshchuk <lalelya7@gmail.com>
2026-09-04 07:59:27 +0200

    feat: implement core guessing game

--- Program.cs
+++ Program.cs
@@ -1,1 +1,44 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace GuessANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int secretNumber = random.Next(1, 101);
+
+            while (true)
+            {
+                Console.Write("Enter your guess (1-100): ");
+                string? input = Console.ReadLine();
+
+                bool isValidNumber = int.TryParse(input, out int guess);
+
+                if (!isValidNumber)
+                {
+                    Console.WriteLine("Invalid input. Please enter a whole number.");
+                }
+                else if (guess < 1 || guess > 100)
+                {
+                    Console.WriteLine("Invalid input. Please enter a number between 1 and 100.");
+                }
+                else
+                {
+                    if (guess < secretNumber)
+                    {
+                        Console.WriteLine("The correct number is higher.");
+                    }
+                    else if (guess > secretNumber)
+                    {
+                        Console.WriteLine("The correct number is lower.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Correct! You guessed the number.");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
